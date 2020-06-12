0b5281d74a46a48b5097a1fcc938dbeee66df978
Neil Mehta <neil.mehta310501@gmail.com>
2020-06-12 17:10:37 +0530

    made the main page scrollable so as to fit the screen size of different mobiles

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -12,92 +12,98 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildAppBar(),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Container(
-            padding: EdgeInsets.only(
-              top: 20,
-              left: 20,
-              right: 20,
-              bottom: 40,
-            ),
-            width: double.infinity,
-            decoration: BoxDecoration(
-              color: kPrimaryColor.withOpacity(0.03),
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(50),
-                bottomRight: Radius.circular(50),
+      body: ListView(
+        scrollDirection: Axis.vertical,
+        cacheExtent: 100.0,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.only(
+                  top: 20,
+                  left: 20,
+                  right: 20,
+                  bottom: 40,
+                ),
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: kPrimaryColor.withOpacity(0.03),
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(50),
+                    bottomRight: Radius.circular(50),
+                  ),
+                ),
+                child: (Wrap(
+                  runSpacing: 20,
+                  spacing: 20,
+                  children: <Widget>[
+                    InfoCard(
+                      title: "Confirmed Cases",
+                      effectedNum: 1062,
+                      iconColor: Color(0xFFFF8C00),
+                      press: () {},
+                    ),
+                    InfoCard(
+                      title: "Total Deaths",
+                      effectedNum: 75,
+                      iconColor: Color(0xFFFF2D55),
+                      press: () {},
+                    ),
+                    InfoCard(
+                      title: "Total Recovered",
+                      effectedNum: 689,
+                      iconColor: Color(0xFF50E3C2),
+                      press: () {},
+                    ),
+                    InfoCard(
+                      title: "New Cases",
+                      effectedNum: 75,
+                      iconColor: Color(0xFF5856D6),
+                      press: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) {
+                              return DetailsScreen();
+                            },
+                          ),
+                        );
+                      },
+                    ),
+                  ],
+                )),
               ),
-            ),
-            child: (Wrap(
-              runSpacing: 20,
-              spacing: 20,
-              children: <Widget>[
-                InfoCard(
-                  title: "Confirmed Cases",
-                  effectedNum: 1062,
-                  iconColor: Color(0xFFFF8C00),
-                  press: () {},
-                ),
-                InfoCard(
-                  title: "Total Deaths",
-                  effectedNum: 75,
-                  iconColor: Color(0xFFFF2D55),
-                  press: () {},
-                ),
-                InfoCard(
-                  title: "Total Recovered",
-                  effectedNum: 689,
-                  iconColor: Color(0xFF50E3C2),
-                  press: () {},
-                ),
-                InfoCard(
-                  title: "New Cases",
-                  effectedNum: 75,
-                  iconColor: Color(0xFF5856D6),
-                  press: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) {
-                          return DetailsScreen();
-                        },
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        "Preventions",
+                        style: Theme.of(context)
+                            .textTheme
+                            .title
+                            .copyWith(fontWeight: FontWeight.bold),
                       ),
-                    );
-                  },
+                      SizedBox(
+                        height: 20,
+                      ),
+                      buildPrevention(),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      buildHelpCard(context),
+                    ],
+                  ),
                 ),
-              ],
-            )),
+              )
+            ],
           ),
-          SizedBox(
-            height: 20,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    "Preventions",
-                    style: Theme.of(context)
-                        .textTheme
-                        .title
-                        .copyWith(fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  buildPrevention(),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  buildHelpCard(context),
-                ],
-              ),
-            ),
-          )
         ],
       ),
     );
