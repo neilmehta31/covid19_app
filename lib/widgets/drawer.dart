6e519e8797e695f0212d84b0bbdb90c4a12441d2
Neil Mehta <neil.mehta310501@gmail.com>
2020-06-30 01:53:24 +0530

    organised the widgets that were all in the home_screen.dart file

---
+++ lib/widgets/drawer.dart
@@ -0,0 +1,212 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+
+
+class DrawerWidget extends StatelessWidget {
+  const DrawerWidget({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.72,
+      child: Drawer(
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+          ),
+          child: ListView(
+            // Important: Remove any padding from the ListView.
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              //HEADER of Drawer
+              DrawerHeader(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage('assets/icons/mask.jpg'),
+                      fit: BoxFit.cover),
+                  color: Colors.white,
+                ),
+                child: null,
+              ),
+              ExpansionTile(
+                leading: Icon(
+                  Icons.add_call,
+                  color: Colors.green,
+                ),
+                title: Text('NMC Helpline'),
+                subtitle: Text(
+                  'Nagpur Municipal Corporation helpline for queries related to COVID-19',
+                  style: TextStyle(color: Colors.grey[500]),
+                ),
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 15.0, vertical: 8.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        border:
+                            Border(bottom: BorderSide(color: Colors.grey[350])),
+                      ),
+                      child: ListTile(
+                        leading: Icon(
+                          Icons.call,
+                          color: Colors.blue,
+                        ),
+                        title: Text('0712-2567021'),
+                        onTap: () {
+                          // Update the state of the app.
+                          // ...
+                          launch("tel:0712-2567021");
+                        },
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 15.0, vertical: 8.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        border:
+                            Border(bottom: BorderSide(color: Colors.grey[350])),
+                      ),
+                      child: ListTile(
+                        leading: Icon(
+                          Icons.call,
+                          color: Colors.blue,
+                        ),
+                        title: Text('0712-2551866'),
+                        onTap: () {
+                          // Update the state of the app.
+                          // ...
+                          launch("tel:0712-2551866");
+                        },
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 15.0, vertical: 8.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        border:
+                            Border(bottom: BorderSide(color: Colors.grey[350])),
+                      ),
+                      child: ListTile(
+                        leading: Icon(
+                          Icons.call,
+                          color: Colors.blue,
+                        ),
+                        title: Text('18002333764'),
+                        onTap: () {
+                          // Update the state of the app.
+                          // ...
+                          launch("tel:18002333764");
+                        },
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Divider(),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 15.0, vertical: 8.0),
+                child: Container(
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.call,
+                      color: Colors.blue,
+                    ),
+                    title: Text('Call GOVT. Helpline'),
+                    subtitle: Text(
+                      'Health ministry toll-free helpline for queries related to COVID-19',
+                      style: TextStyle(color: Colors.grey[500]),
+                    ),
+                    onTap: () {
+                      launch('tel:1075');
+                      // Update the state of the app.
+                      // ...
+                    },
+                  ),
+                ),
+              ),
+              Divider(),
+              GestureDetector(
+                child: ListTile(
+                  leading: Icon(
+                    Icons.contact_mail,
+                    color: Colors.lightBlue,
+                  ),
+                  title: Text('E-pass to travel out of Nagpur'),
+                  onTap: () {
+                    _launchEpass();
+                  },
+                ),
+              ),
+              Center(
+                child: Container(
+                  child: Align(
+                    alignment: FractionalOffset.bottomCenter,
+                    child: Column(
+                      children: <Widget>[
+                        Divider(),
+                        GestureDetector(
+                          child: ListTile(
+                            leading: Icon(
+                              Icons.code,
+                              color: Colors.deepOrange,
+                            ),
+                            title: Text('Source'),
+                            onTap: () {
+                              _launchSourceURL();
+                            },
+                          ),
+                        ),
+                        GestureDetector(
+                          child: ListTile(
+                            leading: Icon(
+                              Icons.feedback,
+                              color: Colors.deepPurple,
+                            ),
+                            title: Text('Feedback'),
+                            onTap: () {
+                              launch('https://forms.gle/i9AAHmKNZfy7gNKJ8');
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                  alignment: Alignment.bottomCenter,
+                  child: Image.asset('assets/icons/wfh.jpg')),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+_launchSourceURL() async {
+  const url = 'https://bing.com/covid/local/nagpur_maharashtra_india';
+  if (await canLaunch(url)) {
+    await launch(url, forceWebView: true, enableJavaScript: true);
+  } else {
+    throw 'Could not launch $url';
+  }
+}
+
+_launchEpass() async {
+  const url = 'https://covid19.mhpolice.in/';
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
+  }
+}
---
+++ lib/widgets/preventionCard.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../constants.dart';
+
+class PreventionCard extends StatelessWidget {
+  final String svgSrc;
+  final String title;
+  const PreventionCard({
+    Key key,
+    this.svgSrc,
+    this.title,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        SvgPicture.asset(svgSrc),
+        Text(
+          title,
+          style: Theme.of(context)
+              .textTheme
+              .bodyText1
+              .copyWith(color: kPrimaryColor),
+        )
+      ],
+    );
+  }
+}
